e68579fd5beadcecc08f254cfcb2299d670b2aac
Oksana-KV51 <12345-70@mail.ru>
2024-03-22 22:47:43 +0400

    oll cicle

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ SCREEN_HEIGTH = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
 
 pygame.display.set_caption('игра ТИР')
-icon = pygame.image.load('img/photo_2024-03-22_21-35-39.jpg')
+icon = pygame.image.load('img/2024.jpg')
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load('img/target.png')
@@ -21,7 +21,17 @@ color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mous_x, mous_y = pygame.mouse.get_pos()
+            if target_x < mous_x < target_x + target_width and target_y < mous_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGTH - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
 
