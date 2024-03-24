e266de0a9aa241289ca44d37e50cd5e837c32cd4
Oksana-KV51 <12345-70@mail.ru>
2024-03-24 13:02:24 +0400

    add new features

---
+++ 01.py
@@ -0,0 +1,37 @@
+import pygame
+import random
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption('игра ТИР')
+icon = pygame.image.load('img/2024.jpg')
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load('img/target.png')
+target_width = 80
+target_height = 60
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+
+
+running = True
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mous_x, mous_y = pygame.mouse.get_pos()
+            if target_x < mous_x < target_x + target_width and target_y < mous_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
+
+
+pygame.quit()
--- main.py
+++ main.py
@@ -3,36 +3,57 @@ import random
 pygame.init()
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGTH = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+SCREEN_HEIGHT = 800
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('игра ТИР')
 icon = pygame.image.load('img/2024.jpg')
 pygame.display.set_icon(icon)
 
+background_img = pygame.image.load('img/3736.jpg')
 target_img = pygame.image.load('img/target.png')
 target_width = 80
-target_height = 80
+target_height = 60
+
+hit_points = 10
+points_scored = 0
+
+targets = []
+for _ in range(5):
+    target_x = random.randint(0, SCREEN_WIDTH - target_width)
+    target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    targets.append((target_x, target_y))
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
-target_y = random.randint(0, SCREEN_HEIGTH - target_height)
-color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
-
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+#color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
 
 running = True
+clock = pygame.time.Clock()
+
 while running:
-    screen.fill(color)
+    screen.blit(background_img, (0, 0))  # Отобразить фоновое изображение
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mous_x, mous_y = pygame.mouse.get_pos()
-            if target_x < mous_x < target_x + target_width and target_y < mous_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEIGTH - target_height)
-    screen.blit(target_img, (target_x, target_y))
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            for idx, target in enumerate(targets):
+                target_x, target_y = target
+                if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                    targets[idx] = (random.randint(0, SCREEN_WIDTH - target_width), random.randint(0, SCREEN_HEIGHT - target_height))
+                    points_scored += hit_points
+
+    for target in targets:
+        target_x, target_y = target
+        screen.blit(target_img, (target_x, target_y))
+
     pygame.display.update()
 
+    clock.tick(30)
+
+print(f'Summa hitting: {points_scored}')
 
 pygame.quit()
 
